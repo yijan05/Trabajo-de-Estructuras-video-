cc9667b8fec2d5c8745dc1c72fba024e1bf30958
yijan05 <yijanvirguez@gmail.com>
2025-05-15 18:20:24 -0500

    Listas dobles
    Este proyecto implementa una estructura de datos tipo lista doblemente enlazada usando JavaScript.

---
+++ source/listaDoble.js
@@ -0,0 +1,53 @@
+
+import {Nodo} from "./nodo.js";
+class ListaDoble {
+    primero
+    ultimo
+    constructor() {
+        this.primero = null;
+        this.ultimo = null;
+    }
+
+    addNodo(valor) {
+        const nuevoNodo = new Nodo(valor);
+
+        if (this.primero === null) {
+            this.primero = nuevoNodo;
+            this.ultimo = nuevoNodo;
+        } else {
+            this.ultimo.siguiente = nuevoNodo;
+            nuevoNodo.anterior = this.ultimo;
+            this.ultimo = nuevoNodo;
+        }
+    }
+
+    inicioFin() {
+        let pivote = this.primero;
+        while (pivote !== null) {
+            console.log(pivote.valor);
+            pivote = pivote.siguiente;
+        }
+    }
+
+    finInicio() {
+        let pivote = this.ultimo;
+        while (pivote !== null) {
+            console.log(pivote.valor);
+            pivote = pivote.anterior;
+        }
+    }
+
+    buscarNodo(valor) {
+        let pivote = this.primero;
+        while (pivote !== null) {
+            if (pivote.valor === valor) {
+                console.log('El nodo ha sido encontrado');
+                return;
+            }
+            pivote = pivote.siguiente;
+        }
+        console.log('No se encontró el nodo');
+    }
+}
+
+module.exports = ListaDoble;
